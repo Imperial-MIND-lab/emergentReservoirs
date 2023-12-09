c4d259679d79046094c039efee76615bbc4073b3
Hanna Tolle <hanna.m.tolle@gmail.com>
2023-12-09 12:16:11 +0000

    add job script for testrun

---
+++ job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=8:mem=32gb
+#PBS -l walltime=08:00:00
+#PBS -N analysis01A
+#PBS -J 1-20
+
+module load tools/prod
+module load MATLAB/2023a_Update_3
+
+cd $PBS_O_WORKDIR
+
+matlab -nodisplay -nosplash -nodesktop -r "try, run('~/projects/emergentReservoirs/main(${PBS_ARRAY_INDEX},true).m'); catch me, e=getReport(me); fprintf('%s\n', e); end; exit;"
