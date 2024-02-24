4d4b5fc6a67821e6480059abdaa44b66db654bb8
Hanna Tolle <hanna.m.tolle@gmail.com>
2024-02-24 19:24:42 +0000

    add: statistical testing to analysis02G

--- analyses/plotAnalysis02G.m
+++ analyses/plotAnalysis02G.m
@@ -74,13 +74,18 @@ results.alpha = categorical(results.alpha, alphas, alphaNames, 'Ordinal', true);
 environments = categories(results.environment);
 numEnvs = length(environments);
 numAlphas = length(alphas);
-numPops = size(results,1)/(numEnvs*numAlphas);
+% numPops = size(results,1)/(numEnvs*numAlphas);
 
 %% generalisability plots
 % boxplots of P(S) test vs. alpha
 
 % % plot data for populations with the same random seed in the same colour
 % colours = winter(numPops);
+
+% statistics
+globalStats = table('Size', [numEnvs, 2],...
+                    'VariableTypes', {'double', 'double'}, ...
+                    'VariableNames', {'pVal', 'Fstat'});
 
 for env = 1:numEnvs
     % extract environment name
@@ -121,14 +126,83 @@ for env = 1:numEnvs
     %         line(xPos, yPos, 'Color', colours(s,:))
     %     end
     % end
+
+    % test for global effect of alpha (using ANOVA)
+    lm = fitlm(slice, [yName, '~alpha']);
+    globalStats.Properties.RowNames{env} = thisEnv;
+    globalStats.pVal(env)=lm.ModelFitVsNullModel.Pvalue;
+    globalStats.Fstat(env)=lm.ModelFitVsNullModel.Fstat;
+    
+    % post-hoc testing, if global effect is significant
+    if lm.ModelFitVsNullModel.Pvalue<0.05
+        posthocStats = table('Size', [0.5*(numAlphas*(numAlphas-1)), 9],...
+                             'VariableTypes', repmat({'double'}, [1, 9]), ...
+                             'VariableNames', {'alpha1', 'alpha2', 'hedgesg', 'tstat', 'pVal', 'df', 'mean_alpha1','mean_alpha2', 'sd_of_diff'});
+        comp = 1;
+        for ai = 1:numAlphas-1
+            for aj = ai+1:numAlphas
+                x = slice.(yName)(slice.alpha==num2str(alphas(ai)));
+                y = slice.(yName)(slice.alpha==num2str(alphas(aj)));
+                s = mes(x, y, 'hedgesg', 'isDep', 1, 'nBoot', 10000);
+
+                % save stats
+                posthocStats.alpha1(comp) = alphas(ai);
+                posthocStats.alpha2(comp) = alphas(aj);
+                posthocStats.hedgesg(comp) = s.hedgesg;
+                posthocStats.tstat(comp) = s.t.tstat;
+                posthocStats.pVal(comp) = s.t.p;
+                posthocStats.df(comp) = s.t.df;
+                posthocStats.mean_alpha1(comp) = mean(x);
+                posthocStats.mean_alpha2(comp) = mean(y);
+                posthocStats.sd_of_diff(comp) = std(posthocStats.mean_alpha1(comp)-posthocStats.mean_alpha2(comp));
+
+                % increment counter
+                comp = comp+1;
+            end
+        end
+
+        % write post-hoc testing results to disk
+        if saveFigures
+            cd(paths.figures)
+            if ~exist(analysisName, 'dir')
+                mkdir(analysisName)
+            end
+            cd(analysisName)
+            writetable(posthocStats, strcat("generalisability_", thisEnv, "_posthocStatistics.csv"))
+            cd(paths.main)
+
+        % or print post-hoc testing results if no saveFigs=false
+        else
+            disp(strcat(thisEnv, " global generalisability effect p-value=", num2str(lm.ModelFitVsNullModel.Pvalue)))
+            disp(posthocStats)
+        end
+    end
+
     if saveFigures
+        % save figures
         savefigs(fullfile(paths.figures, analysisName), ['generalisability_',thisEnv], true)
         close all
     end
 end
 
+if saveFigures
+    % save global effect statistics
+    cd(paths.figures)
+    if ~exist(analysisName, 'dir')
+        mkdir(analysisName)
+    end
+    cd(analysisName)
+    writetable(globalStats, "generalisability_globalStatistics.csv")
+    cd(paths.main)
+end
+
 %% transfer learning plots
 % boxplots of P(S) averaged across tasks j (j~=i) vs. alpha
+
+% statistics
+globalStats = table('Size', [numEnvs, 2],...
+                    'VariableTypes', {'double', 'double'}, ...
+                    'VariableNames', {'pVal', 'Fstat'});
 
 for env = 1:numEnvs
     % extract environment name
@@ -167,10 +241,71 @@ for env = 1:numEnvs
     title(['transfer learning: ', thisEnv])
     ylabel('mean Pj(S)')
 
+    % test for global effect of alpha (using ANOVA)
+    lm = fitlm(slice, 'psJ~alpha');
+    globalStats.Properties.RowNames{env} = thisEnv;
+    globalStats.pVal(env)=lm.ModelFitVsNullModel.Pvalue;
+    globalStats.Fstat(env)=lm.ModelFitVsNullModel.Fstat;
+    
+    % post-hoc testing, if global effect is significant
+    if lm.ModelFitVsNullModel.Pvalue<0.05
+        posthocStats = table('Size', [0.5*(numAlphas*(numAlphas-1)), 9],...
+                             'VariableTypes', repmat({'double'}, [1, 9]), ...
+                             'VariableNames', {'alpha1', 'alpha2', 'hedgesg', 'tstat', 'pVal', 'df', 'mean_alpha1','mean_alpha2', 'sd_of_diff'});
+        comp = 1;
+        for ai = 1:numAlphas-1
+            for aj = ai+1:numAlphas
+                x = slice.psJ(slice.alpha==num2str(alphas(ai)));
+                y = slice.psJ(slice.alpha==num2str(alphas(aj)));
+                s = mes(x, y, 'hedgesg', 'isDep', 1, 'nBoot', 10000);
+        
+                % save stats
+                posthocStats.alpha1(comp) = alphas(ai);
+                posthocStats.alpha2(comp) = alphas(aj);
+                posthocStats.hedgesg(comp) = s.hedgesg;
+                posthocStats.tstat(comp) = s.t.tstat;
+                posthocStats.pVal(comp) = s.t.p;
+                posthocStats.df(comp) = s.t.df;
+                posthocStats.mean_alpha1(comp) = mean(x);
+                posthocStats.mean_alpha2(comp) = mean(y);
+                posthocStats.sd_of_diff(comp) = std(posthocStats.mean_alpha1(comp)-posthocStats.mean_alpha2(comp));
+
+                % increment counter
+                comp = comp+1;
+            end
+        end
+        % write post-hoc testing results to disk
+        if saveFigures
+            cd(paths.figures)
+            if ~exist(analysisName, 'dir')
+                mkdir(analysisName)
+            end
+            cd(analysisName)
+            writetable(posthocStats, strcat("generalisability_", thisEnv, "_posthocStatistics.csv"))
+            cd(paths.main)
+        
+        % or print post-hoc testing results if no saveFigs=false
+        else
+            disp(strcat(thisEnv, " global transfer learning effect p-value=", num2str(lm.ModelFitVsNullModel.Pvalue)))
+            disp(posthocStats)
+        end
+    end
+
     if saveFigures
         savefigs(fullfile(paths.figures, analysisName), ['transferLearning_',thisEnv], true)
         close all
     end
+end
+
+if saveFigures
+    % save global effect statistics
+    cd(paths.figures)
+    if ~exist(analysisName, 'dir')
+        mkdir(analysisName)
+    end
+    cd(analysisName)
+    writetable(globalStats, "transferLearning_globalStatistics.csv")
+    cd(paths.main)
 end
 
 end
