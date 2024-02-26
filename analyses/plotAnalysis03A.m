f87f2efc82cf36dcaa0807256a6a8a845b78a2e4
Hanna Tolle <hanna.m.tolle@gmail.com>
2024-02-26 23:27:42 +0000

    edit: plot all environments together

--- analyses/plotAnalysis03A.m
+++ analyses/plotAnalysis03A.m
@@ -19,37 +19,100 @@ for file = 1:length(files)
     % load config and results of analysis
     results = load(fullfile(paths.outputs, analysisName, files(file).name)).results;
     config = load(fullfile(paths.outputs, analysisName, files(file).name)).config;
+    numEnvs = length(config.environments);
 
-    % get colours for each environment
-    colours = winter(length(config.environments));
+    % stats output table
+    varNames = {'environment', 'hedgesg', 'tstat', 'pVal', 'fdr', 'df'};
+    stats = table('Size', [numEnvs, length(varNames)],...
+                  'VariableTypes', ['string', repmat({'double'}, [1, length(varNames)-1])], ...
+                  'VariableNames', varNames);
+    
+    % one plot per outcome measure
+    for measure = 1:length(config.resultNames)
 
-    % make plots for each environment and output measure
-    for env = 1:length(config.environments)
-        thisEnv = config.environments{env};
-   
-        % slice the table to get only the results for this env
-        slice = results(results.environment==thisEnv,:);
-        for measure = 1:length(config.resultNames)
-            x = kron([1; 2], ones(size(slice, 1), 1));
-            y = [slice.(['human_',config.resultNames{measure}]);...
-                slice.(['random_',config.resultNames{measure}])];
-            boxSwarmPlot(y, x, 1, colours(env,:));
-            ylabel(config.resultNames{measure})
-            subtitle(thisEnv)
-            set(gca, 'XTick', 1:2, 'XTickLabel', {'human', 'random'});
-        
-            % save plots
-            if saveFigures
-                figname = [analysisName, '_', ...
-                           thisEnv, '_', ...
-                           config.resultNames{measure}, '_', ...
-                           num2str(file)];
-                savefigs(fullfile(paths.figures, analysisName), figname, true)
-                close all
-            end  
+        % extract data to plot
+        x = []; y = []; human = []; d = 0.35; spacing=2;
+        for env = 1:numEnvs
+            thisEnv = config.environments{env};
+
+            % get the human reservoir data
+            yHuman = results(results.environment==thisEnv,:).(['human_',config.resultNames{measure}]);
+            y = [y; yHuman];
+            x = [x; (spacing*env-d)*ones(length(yHuman),1)];
+            human = [human; true(length(yHuman),1)];
+
+            % get the random reservoir data
+            yRandom = results(results.environment==thisEnv,:).(['random_',config.resultNames{measure}]);
+            y = [y; yRandom];
+            x = [x; (spacing*env+d)*ones(length(yRandom),1)];
+            human = [human; ~true(length(yRandom),1)];
+
+            % test for group differences
+            s = mes(yHuman, yRandom, 'hedgesg', 'isDep', 1, 'nBoot', 10000);
+            stats.environment(env) = thisEnv;
+            stats.hedgesg(env) = s.hedgesg;
+            stats.tstat(env) = s.t.tstat;
+            stats.pVal(env) = s.t.p;
+            stats.df(env) = s.t.df;
+        end
+        human = human==1;
+        stats.fdr = fdr(stats.pVal(:));
+
+        % make plot
+        figure();
+        boxchart(x, y, ...
+                 'BoxFaceColor', [1 1 1].*0.35, 'BoxFaceAlpha', 0.15, ...
+                 'LineWidth', 1, 'MarkerStyle', 'none');
+        hold on
+        swarmchart(x(human), y(human), ...
+                   [], 'MarkerFaceColor', [0 21 244]./255, ...
+                   'MarkerFaceAlpha', 0.5, 'MarkerEdgeColor', 'none', ...
+                   'XJitterWidth', 0.35)
+        swarmchart(x(~human), y(~human), ...
+                   [], 'MarkerFaceColor', [0 139 189]./255, ...
+                   'MarkerFaceAlpha', 0.5, 'MarkerEdgeColor', 'none', ...
+                   'XJitterWidth', 0.35)
+        hold off
+        ylabel(config.resultNames{measure})
+        set(gca, 'XTick', (1:numEnvs)*spacing, 'XTickLabel', config.environments);
+
+        if any(stats.fdr(:)<0.05)
+            % scale y-axis limits to make space for asterisks
+            maxVal = max(y);
+            minVal = min(y);
+            yOffset = (maxVal-minVal)*0.15;
+            if maxVal>minVal
+                ylim([minVal-0.5*yOffset maxVal+yOffset])                                
+            end
+            
+            % add asterisks to plot if p<0.05
+            for env = 1:numEnvs
+                asterisk = get_asterisk(stats.fdr(env));
+                label = text(env*spacing, maxVal+0.5*yOffset, asterisk, 'Fontsize', 14);
+                set(label,'HorizontalAlignment','center','VerticalAlignment','middle');
+            end
+        end
+    
+        % save plots
+        if saveFigures
+            figname = [analysisName, '_allEnvs_', ...
+                       config.resultNames{measure}, '_', ...
+                       num2str(file)];
+            savefigs(fullfile(paths.figures, analysisName), figname, true)
+            close all
+            % save statistics
+            cd(paths.figures)
+            if ~exist(analysisName, 'dir')
+                mkdir(analysisName)
+            end
+            cd(analysisName)
+            writetable(stats, [figname,'.csv'])
+            cd(paths.main)
         end
     end
 end
 
 end
+
+
 
