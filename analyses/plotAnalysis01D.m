aa28326fdb52fca037324f9fa36f0b95e6608b5a
Hanna Tolle <hanna.m.tolle@gmail.com>
2024-04-16 10:42:58 +0100

    add: task difficulty statistical testing

--- analyses/plotAnalysis01D.m
+++ analyses/plotAnalysis01D.m
@@ -26,6 +26,11 @@ boxData = struct();
 ignore = ~true(numRows, numEnvs); % ignore reservoirs with P(E)==0 or P(S)==0
 probability_names = {'P(S|E)-P(S)', 'P(S|E)', 'P(E|S)', 'P(S)'}; % probabilities to plot
 
+% extract P(S) of all reservoirs in one table
+successProps = table('Size', [numRows*numEnvs, 2],...
+                     'VariableTypes', {'double', 'categorical'},...
+                     'VariableNames', {'success', 'environment'});
+
 for env = 1:numEnvs
     % get name of environment
     thisEnv = environments{env};
@@ -56,6 +61,10 @@ for env = 1:numEnvs
     tbl.("P(S)") = ps;
     boxData.(thisEnv) = tbl;
 
+    % store success probability of all reservoirs
+    successProps.environment((numRows*(env-1)+1):numRows*env) = thisEnv;
+    successProps.success((numRows*(env-1)+1):numRows*env) = results.(['ps',thisEnv]);
+
 end
 
 %% statistical tests
@@ -66,7 +75,7 @@ if saveFigures
     catNames = {'environment', 'randomVariable'};
     statsNames = {'tstat', 'hedgesg', 'df', 'sd', 'mean', 'median', 'pVal', 'fdr'};
     numStats = length(statsNames);
-    stats = table('Size', [numEnvs*(length(probability_names)-1), numStats+length(catNames)], ...
+    stats = table('Size', [numEnvs*(length(probability_names)), numStats+length(catNames)], ...
                   'VariableTypes', [repmat({'categorical'}, [1 length(catNames)]), repmat({'double'}, [1 numStats])], ...
                   'VariableNames', [catNames(:)', statsNames(:)']);
     
@@ -122,6 +131,19 @@ if saveFigures
         stats.mean(row) = mean(boxData.(thisEnv).("P(E|S)"));
         stats.median(row) = median(boxData.(thisEnv).("P(E|S)"));
         row = row+1;
+
+        % TEST 04:
+        % Just store stats of P(S) distribution
+        stats.environment(row) = thisEnv;
+        stats.randomVariable(row) = "P(S)";
+        stats.hedgesg(row) = nan;
+        stats.tstat(row) = nan;
+        stats.df(row) = nan;
+        stats.pVal(row) = nan;
+        stats.sd(row) = std(boxData.(thisEnv).("P(S)"));
+        stats.mean(row) = mean(boxData.(thisEnv).("P(S)"));
+        stats.median(row) = median(boxData.(thisEnv).("P(S)"));
+        row = row+1;
         
     end
     
@@ -138,11 +160,93 @@ if saveFigures
     cd(paths.main)
 end
 
+%% statistical testing 2
+% Test for group differences in P(S) between environments
+
+if saveFigures
+    % stats of task difficulty test
+    globalStats = table('Size', [1+numEnvs*(numEnvs-1)*0.5, 4],...
+                        'VariableTypes', {'string', 'string', 'double', 'double'}, ...
+                        'VariableNames', {'env1', 'env2' 'pVal', 'Fstat'});
+    
+    % test for global effect of alpha (using ANOVA)
+    lm = fitlm(successProps, 'success ~ environment');
+    globalStats.pVal(1)=lm.ModelFitVsNullModel.Pvalue;
+    globalStats.Fstat(1)=lm.ModelFitVsNullModel.Fstat;
+    globalStats.env1(1) = 'global';
+    globalStats.env2(1) = 'global';
+    
+    % post-hoc testing, if global effect is significant
+    if lm.ModelFitVsNullModel.Pvalue<0.05
+        varNames = {'env1', 'env2', 'hedgesg', 'tstat', 'pVal', 'fdr', 'df', 'mean1','mean2', 'sd1', 'sd2'};
+        posthocStats = table('Size', [numEnvs*(numEnvs-1)*0.5, length(varNames)],...
+                             'VariableTypes', [{'string'}, {'string'}, repmat({'double'}, [1, length(varNames)-2])], ...
+                             'VariableNames', varNames);
+        
+        comp = 1;
+        for envi = 1:numEnvs-1
+            for envj = envi+1:numEnvs
+                x = successProps.success(successProps.environment==environments{envi});
+                y = successProps.success(successProps.environment==environments{envj});
+                s = mes(x, y, 'hedgesg', 'isDep', 1, 'nBoot', 10000);
+    
+                % save stats
+                posthocStats.env1(comp) = environments{envi};
+                posthocStats.env2(comp) = environments{envj};
+                posthocStats.hedgesg(comp) = s.hedgesg;
+                posthocStats.tstat(comp) = s.t.tstat;
+                posthocStats.pVal(comp) = s.t.p;
+                posthocStats.df(comp) = s.t.df;
+                posthocStats.mean1(comp) = mean(x);
+                posthocStats.mean2(comp) = mean(y);
+                posthocStats.sd1(comp) = std(x);
+                posthocStats.sd2(comp) = std(y);
+    
+                % increment counter
+                comp = comp+1;
+            end
+        end
+
+        % fdr correction
+        posthocStats.fdr = fdr(posthocStats.pVal(:));
+
+        % save statistics
+        cd(paths.figures)
+        if ~exist(analysisName, 'dir')
+            mkdir(analysisName)
+        end
+        cd(analysisName)
+        writetable(posthocStats, strcat(analysisName,"_statistics_successprob_posthoc.csv"))
+        cd(paths.main)
+    end
+
+    % save statistics
+    cd(paths.figures)
+    if ~exist(analysisName, 'dir')
+        mkdir(analysisName)
+    end
+    cd(analysisName)
+    writetable(globalStats, strcat(analysisName,"_statistics_successprob_global.csv"))
+    cd(paths.main)
+end
+
+% plot all environments together
+figure();
+boxchart(successProps.environment, successProps.success, ...
+         'BoxFaceColor', [1 1 1].*0.35, 'BoxFaceAlpha', 0.15, ...
+         'LineWidth', 1, 'MarkerStyle', 'none');
+ylabel('P(S) all')
+
+if saveFigures
+    savefigs(fullfile(paths.figures, analysisName), 'success_probabilities', true)
+    close all
+end
+
 %% plotting
 % boxplots with P(S|E)-P(S)
 
 % boxplot with all environments
-for pn = 1:length(probability_names)-1
+for pn = 1:length(probability_names)
     plotting(environments, ignore, probability_names{pn})
     if saveFigures
         savefigs(fullfile(paths.figures, analysisName), probability_names{pn}, true)
@@ -208,7 +312,6 @@ end
         start = 1; 
         for i = 1:thisNumEnvs
             thisEnv = environments{feasibility_index(i)};
-            disp(thisEnv)
         
             % get data to plot
             stop = start+length(boxData.(thisEnv).(probName))-1;
